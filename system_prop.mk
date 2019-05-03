386495ac77372561a5fa81fb159b76c1c1a9d78d
Peiyong Lin <lpy@google.com>
2019-05-03 16:35:02 -0700

    raphael: Turn on GPU protected composition.
    Change-Id: Ie385081961d8b89b81a034178d10176197b639d5
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -59,6 +59,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.has_wide_color_display=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=2 \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
+    ro.surface_flinger.protected_contents=true \
     ro.surface_flinger.set_idle_timer_ms=80 \
     ro.surface_flinger.set_touch_timer_ms=200 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
