4259bd7dbe08a9581e76eae46bc1dbbaa58374e3
Sagar Shah <sagarshah2326@gmail.com>
2019-11-11 18:16:50 +0100

    raphael: Refactor BoardConfigQcom to ProductConfigQcom
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- device.mk
+++ device.mk
@@ -46,6 +46,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_configuration.xml
 
+# Board
+PRODUCT_USES_QCOM_HARDWARE := true
+PRODUCT_BOARD_PLATFORM := msmnile
+
 # Camera
 PRODUCT_PACKAGES += \
     Snap
