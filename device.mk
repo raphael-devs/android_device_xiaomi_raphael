f4a2bafdb36a05ca87c8643d4d2d901976cadf4f
UtsavisGreat <utsavbalar1231@gmail.com>
2019-11-23 16:49:26 +0000

    raphael: import perfboost config from hotdog
    but keep Xiaomi's Bluetooth Boost
    
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- device.mk
+++ device.mk
@@ -128,7 +128,8 @@ PRODUCT_COPY_FILES += \
 
 # Perf
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/msm_irqbalance.conf
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/msm_irqbalance.conf \
+    $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/perf/perfboostsconfig.xml
 
 # Power
 PRODUCT_PACKAGES += \
