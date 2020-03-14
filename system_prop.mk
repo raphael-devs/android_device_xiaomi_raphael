c0837afbb90ef5a0fa392afb1eff0d081cc6aa4f
Lucas Dupin <dupin@google.com>
2020-03-14 14:44:40 -0700

    raphael: Enable blurs, under dev-option
    Test: make
    Test: manual
    Bug: 149792636
    Change-Id: I3352b34739f5f45efa4088044de0e030449ff866
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -47,6 +47,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.fod.modified.dc_status=false \
     sys.displayfeature.hbm.enable=true
 
+# Enable blurs, hidden under dev option
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.surface_flinger.supports_background_blur=1 \
+    persist.sys.sf.disable_blurs=1 \
+    ro.sf.blurs_are_expensive=1
+
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.latch_unsignaled=1 \
