76568bf9ca81fa799298667756e75e102d2f10f5
Ayush Dubey <ayushdubey70@gmail.com>
2020-09-13 12:02:07 +0530

    raphael: bringup statix
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- statix_raphael.mk
+++ statix_raphael.mk
@@ -6,14 +6,14 @@
 
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/statix/config/common.mk)
+$(call inherit-product, vendor/statix/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := raphael
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_raphael
+PRODUCT_NAME := statix_raphael
 
 BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFKINXM:user/release-keys"
 
