62591a79f40e95d6cfedf4487b83ebda12deee32
Ayush Dubey <ayushdubey70@gmail.com>
2020-09-23 20:24:48 +0530

    raphael: fix init build for R
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- init/init_raphael.cpp
+++ init/init_raphael.cpp
@@ -19,10 +19,9 @@
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
-#include "property_service.h"
 #include "vendor_init.h"
 
-using android::init::property_set;
+using ::android::base::SetProperty;
 
 void property_override(char const prop[], char const value[])
 {
