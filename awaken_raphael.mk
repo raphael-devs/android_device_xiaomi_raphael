4eeedb40b717142b7be36ec2871fe055fbacc6b0
Ayush Dubey <ayushdubey70@gmail.com>
2020-10-13 20:27:45 +0530

    raphael: awaken: switch to official build type
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- awaken_raphael.mk
+++ awaken_raphael.mk
@@ -18,3 +18,5 @@ PRODUCT_NAME := awaken_raphael
 BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFKINXM:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+AWAKEN_BUILD_TYPE := OFFICIAL
