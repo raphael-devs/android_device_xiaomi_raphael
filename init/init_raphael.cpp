c2d227058f7db6425d0db3305b7466356a755fe5
Ayush Dubey <ayushdubey70@gmail.com>
2020-10-18 20:29:04 +0530

    raphael: init: override build description to pass Safetynet
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- init/init_raphael.cpp
+++ init/init_raphael.cpp
@@ -59,21 +59,21 @@ void load_raphaelglobal() {
     property_override("ro.product.model", "Mi 9T Pro");
     property_override("ro.build.product", "raphael");
     property_override("ro.product.device", "raphael");
-    property_override("ro.build.description", "raphael-user 10 QKQ1.190825.002 V11.0.3.0.QFKEUXM release-keys");
+    property_override("ro.build.description", "coral-user 11 RP1A.201005.004 6782484 release-keys");
 }
 
 void load_raphaelin() {
     property_override("ro.product.model", "Redmi K20 Pro");
     property_override("ro.build.product", "raphaelin");
     property_override("ro.product.device", "raphaelin");
-    property_override("ro.build.description", "raphaelin-user 10 QKQ1.190825.002 V11.0.1.0.QFKINXM release-keys");
+    property_override("ro.build.description", "coral-user 11 RP1A.201005.004 6782484 release-keys");
 }
 
 void load_raphael() {
     property_override("ro.product.model", "Redmi K20 Pro");
     property_override("ro.build.product", "raphael");
     property_override("ro.product.device", "raphael");
-    property_override("ro.build.description", "raphael-user 10 QKQ1.190825.002 V11.0.2.0.QFKCNXM release-keys");
+    property_override("ro.build.description", "coral-user 11 RP1A.201005.004 6782484 release-keys");
 }
 
 
