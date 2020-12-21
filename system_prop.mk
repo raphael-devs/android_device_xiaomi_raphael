0dbba32db0aac8c7cadd725db3c88fb02f86b788
Ayush Dubey <ayushdubey70@gmail.com>
2020-12-21 19:47:16 +0530

    raphael: fix spacing between keyboard and navbar
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -52,6 +52,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1 \
     ro.sf.blurs_are_expensive=1
 
+# Gboard
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.com.google.ime.kb_pad_port_b=1
+
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.disable_backpressure=1 \
